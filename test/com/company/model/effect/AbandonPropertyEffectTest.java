48d107e65883b256dda6736e00f70ba030d2a88a
Jacky Ng <npsp2142@gmail.com>
2021-11-01 02:01:52 +0800

    Added empty tests.

---
+++ test/com/company/model/effect/AbandonPropertyEffectTest.java
@@ -0,0 +1,17 @@
+package com.company.model.effect;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbandonPropertyEffectTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void onLand() {
+    }
+}
