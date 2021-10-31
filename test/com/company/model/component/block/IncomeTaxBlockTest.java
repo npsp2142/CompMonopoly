90585735be05cbeaf4a86e5280aa98191b3e40af
Jacky Ng <npsp2142@gmail.com>
2021-10-31 23:20:03 +0800

    Added Javadoc of IncomeTaxBlockTest

--- test/com/company/model/component/block/IncomeTaxBlockTest.java
+++ test/com/company/model/component/block/IncomeTaxBlockTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Unit tests of IncomeTaxBlock
+ */
 class IncomeTaxBlockTest {
 
     Player player;
