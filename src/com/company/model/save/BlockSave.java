424d8e7c0d2ff68c38e51ab0904e0534e711ccfc
wongthomas1030 <wongthomas1030@gmail.com>
2021-11-02 00:46:28 +0800

    Added GameBoard and GameSystemTest

--- src/com/company/model/save/BlockSave.java
+++ src/com/company/model/save/BlockSave.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class BlockSave implements Serializable {
     private final String name;
 
+
     public BlockSave(String name) {
         this.name = name;
     }
--- test/com/company/model/GameSystemTest.java
+++ test/com/company/model/GameSystemTest.java
@@ -1,17 +1,38 @@
 package com.company.model;
 
+import com.company.model.component.Player;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test Case for GameSystemTest
+ */
 class GameSystemTest {
+    private final ArrayList<Player> players;
+    private Player currentPlayer;
 
-    @Test
-    void startGame() {
+    GameSystemTest(ArrayList<Player> players) {
+        this.players = players;
     }
 
+    /**
+     * Test Case for GameSystemTest.startGame()
+     */
+    @Test
+    void startGame() {
+        assertEquals(CompMonopolyApplication.Status.PLAYING,CompMonopolyApplication.instance.getStatus());//Test Game status
+    }
+
+    /**
+     * Test Case for GameSystemTest.endTurn()
+     */
     @Test
     void endTurn() {
+        assertEquals(CompMonopolyApplication.Status.MENU,CompMonopolyApplication.instance.getStatus());// Test Game status when only 1 player left or 100 rounds ended.
+        assertEquals(players.get(0),currentPlayer );//Test if the player is the last player
     }
 
     @Test
