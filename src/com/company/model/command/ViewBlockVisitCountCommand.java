bebe6149b22fe370a1f1c6bbad0988e3629ffdd0
wongthomas1030 <wongthomas1030@gmail.com>
2021-11-18 23:47:23 +0800

    Added Javadoc for TeleportCommand, ViewAmountCommand, ViewBlockVisitCountCommand, ViewBoardCommand, ViewPathCommand, ViewPersonalProfileCommand

--- src/com/company/model/command/ViewBlockVisitCountCommand.java
+++ src/com/company/model/command/ViewBlockVisitCountCommand.java
@@ -3,9 +3,15 @@ package com.company.model.command;
 import com.company.model.CompMonopolyApplication;
 import com.company.model.observer.BlockVisitObserver;
 
+/**
+ * When player wants to see how many times they pass the blocks
+ */
 public class ViewBlockVisitCountCommand implements Command {
     private final BlockVisitObserver blockVisitObserver;
 
+    /**
+     * @param blockVisitObserver the counter for the blocks the player passed
+     */
     public ViewBlockVisitCountCommand(BlockVisitObserver blockVisitObserver) {
         this.blockVisitObserver = blockVisitObserver;
     }
