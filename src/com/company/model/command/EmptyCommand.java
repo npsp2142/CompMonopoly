c09f6bd0814b28fba3f01fa173a03d0f9fb747a9
wongthomas1030 <wongthomas1030@gmail.com>
2021-11-18 23:09:59 +0800

    Added Javadoc for Empty Command

--- src/com/company/model/command/EmptyCommand.java
+++ src/com/company/model/command/EmptyCommand.java
@@ -2,9 +2,15 @@ package com.company.model.command;
 
 import com.company.model.GameDisplay;
 
+/**
+ * When user input is empty, will show some messages
+ */
 public class EmptyCommand implements Command {
     private final String description;
 
+    /**
+     * @param description is messages
+     */
     public EmptyCommand(String description) {
         this.description = description;
     }
